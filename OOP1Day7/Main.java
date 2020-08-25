60c4a1a12901736a92845e51f359cef55cc2790f
moritzdp <69892518+moritzdp@users.noreply.github.com>
2020-08-26 00:01:50 +0800

    Create Main.java

---
+++ OOP1Day7/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    //System.out.println("Hello world!");
+
+    Parrot parrotObj = new Parrot();
+    parrotObj.fly();
+    parrotObj.sleep();
+
+
+    Penguin penguinObj = new Penguin();
+    penguinObj.swim();
+    penguinObj.eat();
+
+
+
+
+  }
+}
