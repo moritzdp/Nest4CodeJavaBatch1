14fd40d422cf04bc307c141ce3b348a3806682f2
moritzdp <69892518+moritzdp@users.noreply.github.com>
2020-08-26 00:01:23 +0800

    Create FlyBehavior.java

---
+++ OOP1Day7/FlyBehavior.java
@@ -0,0 +1,23 @@
+public abstract class Bird {
+  int height;
+  int weight;
+  String status;
+  int sound; 
+
+
+// whether birds sound same
+  public abstract void sound();
+  public abstract void swim();
+// whether birds fly same
+  public abstract void fly();
+
+  public void eat() {
+    System.out.println("Bird is eating");
+  }
+
+  public void sleep() {
+    System.out.println("Bird is sleeping");
+  }
+
+
+}
